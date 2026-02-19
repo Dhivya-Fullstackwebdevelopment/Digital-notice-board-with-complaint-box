85a287000ac8817f4001d8474b294de71d284360
Dhivya-Fullstackwebdevelopment <divya.p9842@gmail.com>
2026-02-19 14:41:26 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import "./index.css"
+
+function App() {
+  return (
+    <div className="bg-green-500 text-amber-200 text-4xl  flex items-center justify-center">
+      Tailwind v4 Worklkjhgfdc🚀
+    </div>
+  );
+}
+
+export default App;
+
+
+
